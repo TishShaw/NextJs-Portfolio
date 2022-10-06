5b0e2e2f5e362b3e052558ba6b5ad66b27c9d65f
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-10-06 08:58:13 -0400

    add initial navbar and hero

---
+++ portfolio/components/Box.tsx
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+import React, { useRef } from 'react'
+import { useTexture } from "@react-three/drei"
+import { useFrame, ThreeElements } from '@react-three/fiber'
+
+type Props = {}
+
+const Box = (props:ThreeElements['mesh']) => {
+    const mesh = useRef<THREE.Mesh>(null!)
+
+    useFrame((state, delta) => ( mesh.current.rotation.x = 100,  mesh.current.rotation.y = 100,  mesh.current.rotation.z += .03))
+
+    const imgProps = useTexture({
+        map: "/_next/static/media/map.fcec4c14.jpg"
+    })
+
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={2}>
+            <torusGeometry args={[2, .2, 16, 100]}  />
+            <pointLight position={[2, 3, 4]} />
+            <meshStandardMaterial  {...imgProps} />
+        </mesh>
+    )
+}
+
+export default Box
---
+++ portfolio/components/Hero.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Typewriter from 'typewriter-effect';
+import { Canvas } from "@react-three/fiber";
+import css from "../styles/Home.module.css";
+import Box from "./Box";
+import Link from 'next/link';
+
+type Props = {}
+
+const Hero = (props: Props) => {
+
+    return (
+        <div className={css.scene}>
+            <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+                <div className="top-50 text-white absolute z-10">
+                    <div className="">
+                        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software engineer</h2>
+                    </div>
+                    <span className="text-5xl font-semibold lg:text-6xl pb-2 px-10">
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    "Hi, my name is Tishtanya Shaw",
+                                    "I am a web developer",
+                                    "Let's work together :)!",
+                                ],
+                                autoStart: true,
+                                loop: true,
+                                delay: 70,
+                            }}
+                            />
+                    </span>
+                    <div className="">
+                        <Link href="#about"><button className="heroBtn">About</button></Link>
+                        <Link href="#experience"><button className="heroBtn">Experience</button></Link>
+                        <Link href="#skills"><button className="heroBtn">Skills</button></Link>
+                        <Link href="#projects"><button className="heroBtn">Projects</button></Link>
+                    </div>
+                </div>
+                    <Canvas
+                        shadows={true}
+                        className={css.canvas}
+                        camera={{
+                        position: [-6, 7, 7],
+                        }}
+                    >
+                        <ambientLight />
+                        <Box  />
+                    </Canvas>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
