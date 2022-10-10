dbebc979f2a971f014d4b0fec12edb6113f1e3db
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-10-09 20:19:41 -0400

    Update navbar icons and add skills animation

--- portfolio/components/Skill.tsx
+++ portfolio/components/Skill.tsx
@@ -5,13 +5,15 @@ import { urlFor } from '../sanity'
 
 type Props = {
     skill: Skill
+    directionLeft: boolean
 }
 
-const SkillCard = ({skill}: Props) => {
+const SkillCard = ({skill, directionLeft}: Props) => {
     return (
         <div className="group relative flex cursor-pointer">
             <motion.img
             initial={{
+                x: directionLeft ? -200 : 200,
                 opacity: 0
             }}
             transition={{ duration: 1 }}
@@ -19,6 +21,11 @@ const SkillCard = ({skill}: Props) => {
             src={urlFor(skill?.image).url()}
             className='rounded-full border border-gray-500 object-cover h-20 w-20 p-4 md:w-24 md:h-24 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out'
             />
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 p-4 md:w-24 md:h-24 xl:h-32 xl:w-32 rounded-full z-0 ">
+                <div className="flex items-center justify-center h-full">
+                    <p className="text-xl font-bold text-black opacity-100 flex justify-center items-center mx-auto text-center absolute">{skill.title}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- portfolio/components/Skills.tsx
+++ portfolio/components/Skills.tsx
@@ -12,13 +12,19 @@ const Skills = ({skills}: Props) => {
         <div
         className="h-screen flex relative flex-col text-center md:text-left xl:flex-row xl:px-10 md:min-h-screen justify-center xl:space-y-0 mx-auto items-center ">
             <h3 className="absolute text-2xl text-gray-500 top-24 uppercase tracking-[20px]">Skills</h3>
-            <h3 className="absolute top-[150px] uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for current proficency</h3>
+            <h3 className="absolute top-[150px] uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for skill details</h3>
             <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-8 md:p-4 mt-40 md:mt-50 lg:mt-24 xl:pt-40">
                 {
-                    skills.map((skill) => (
+                    skills?.slice(0, skills.length /2).map((skill) => (
                         <SkillCard key={skill._id} skill={skill} />
                     ))
                 }
+                {
+                    skills?.slice(skills.length / 2, skills.length).map((skill) => (
+                        <SkillCard key={skill._id} skill={skill} directionLeft/>
+                    ))
+                }
+
             </div>
         </div>
     )
